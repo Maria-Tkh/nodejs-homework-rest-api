0024c11a098428cd4e07ab1e7f2ee3b4549b3e90
Maria-Tkh <mcweaver117@gmail.com>
2021-12-13 15:44:40 +0200

    comments fixed

--- controllers/contacts/updateContact.js
+++ controllers/contacts/updateContact.js
@@ -1,13 +1,7 @@
 const contactsOperations = require('../../model/contacts')
 const { NotFound } = require('http-errors')
-// const { BadRequest } = require('http-errors')
 
 const updateContact = async (req, res) => {
-//   try {
-//   const { error } = contactSchema.validate(req.body)
-//   if (error) {
-//     throw new BadRequest('Missing required field')
-//   }
   const { id } = req.params
   const result = await contactsOperations.updateContact(id, req.body)
   if (!result) {
@@ -20,8 +14,5 @@ const updateContact = async (req, res) => {
       result
     }
   })
-//   } catch (error) {
-//     next(error)
-//   }
 }
 module.exports = updateContact
